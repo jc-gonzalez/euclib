bda9e10fc50d9a2ea25fc9985ccf3a022c1a4771
J C Gonzalez <JCGonzalez@sciops.esa.int>
2019-04-05 14:19:18 +0200

    Avoid printing internal value

--- fnamespec/fnamespec/__init__.py
+++ fnamespec/fnamespec/__init__.py
@@ -5,4 +5,4 @@ if 'LOGGING_MODULES' in os.environ:
 add_modules = [ __name__ + '.' + x for x in
                 ['fnamespec', 'fnamespec.ProductMetadata']]
 os.environ['LOGGING_MODULES'] = logging_modules + ':'.join(add_modules)
-print(os.environ['LOGGING_MODULES'])
+#print(os.environ['LOGGING_MODULES'])
