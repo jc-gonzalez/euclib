9f92f5631aa763014945f9b4cfdc18baa8d99539
J C Gonzalez <JCGonzalez@sciops.esa.int>
2019-07-08 08:19:17 +0200

    New module to read OSS obs.id. info and generate table to append to FITS files

---
+++ ess/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+logging_modules = ''
+if 'LOGGING_MODULES' in os.environ:
+    logging_modules = os.environ['LOGGING_MODULES'] + ':'
+add_modules = [__name__ + '.' + x for x in
+               ['oss_reader']]
+os.environ['LOGGING_MODULES'] = logging_modules + ':'.join(add_modules)
+# print(os.environ['LOGGING_MODULES'])
