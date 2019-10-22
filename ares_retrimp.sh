69406a3d1f30bdfc6af4a16f53c69006185f3add
J C Gonzalez <JCGonzalez@sciops.esa.int>
2019-10-22 09:20:00 +0200

    Adding SCRIPTPATH

--- ares_retrimp.sh
+++ ares_retrimp.sh
@@ -6,7 +6,8 @@
 # Created by J C Gonzalez <jcgonzalez@sciops.esa.int>
 # Copyright (C) 2019 by Euclid SOC Team
 #------------------------------------------------------------------------------------
+SCRIPTPATH=$(dirname $(stat -f $0))
 export PYARES_INI_FILE=/home/ares/.config/aresri/retrieval_config.ini
-export PYTHONPATH=$(pwd) 
-python3 ./apps/ares_retrimp/ares_retrimp.py
+export PYTHONPATH=${SCRIPTPATH} 
+python3 ${SCRIPTPATH}/apps/ares_retrimp/ares_retrimp.py
 
